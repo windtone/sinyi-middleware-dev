4c06e4cc129ce87d9d03b0db9e66e14acff31a33
Tonny Wang <tonny.wang@thinkpower-info.com.tw>
2020-02-11 13:23:19 +0800

    add debug nogps

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/', (req, res, next) => {
 	let headers = {
 		'Content-Type': 'application/json',
 	}
-	console.log(content.originalDetectIntentRequest.payload.inputs[0].rawInputs)
+	console.log(content.originalDetectIntentRequest)
 
 	if (conv.fallbackCancelGPS(content)) {
 		repText = '@relisten'
